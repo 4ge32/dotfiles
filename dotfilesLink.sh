9dd7f704ba4367782efcd8af23065329c129e80a
Fumiya Shigemitsu <gyro.scaremonger@gmail.com>
2017-08-28 01:53:08 +0900

    installer script: modify dotfilesLink.sh
    Improve to install dein firstly

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
+sh ./installer.sh ~/.vim/dein
+
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.vim ~/.vim
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
