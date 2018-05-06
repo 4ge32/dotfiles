60ead6da7c995c871ec4264a2918b34ea854aeba
Fumiya Shigemitsu <gyro.scaremonger@gmail.com>
2018-05-06 07:56:49 -0700

    fix copy source

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+sudo apt install -y build-essential 
+sudo apt install -y curl
+sudo apt install -y vim
+sudo apt install -y byobu
 
 curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
 sh ./installer.sh ~/.vim/dein
@@ -11,5 +15,5 @@ ln -sf ~/dotfiles/.byobu ~/.byobu
 
 cp -rv .vim/plugin ~/.vim
 cp -rv .vim/rc  ~/.vim
-cp -v skeleton.c ~/.vim
-cp -v skeleton.cpp ~/.vim
+cp -v .vim/skeleton.c ~/.vim
+cp -v .vim/skeleton.cpp ~/.vim
