ac5c29b9579e1e9b55665dee952dc347bff82d6e
Fumiya Shigemitsu <gyro.scaremonger@gmail.com>
2018-05-15 22:17:56 +0900

    Add function that can judge the kind of distro that is used now.

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,8 +1,42 @@
 #!/bin/sh
-sudo apt install -y build-essential 
-sudo apt install -y curl
-sudo apt install -y vim
-sudo apt install -y byobu
+
+get_os_distribution() {
+    if   [ -e /etc/debian_version ] ||
+         [ -e /etc/debian_release ]; then
+        # Check Ubuntu or Debian
+        if [ -e /etc/lsb-release ]; then
+            # Ubuntu
+            distri_name="ubuntu"
+        fi
+    elif [ -e /etc/fedora-release ]; then
+        # Fedra
+        distri_name="fedora"
+    elif [ -e /etc/gentoo-release ]; then
+        # Gentoo Linux
+        distri_name="gentoo"
+    else
+        # Other
+        echo "unkown distribution"
+        distri_name="unkown"
+    fi
+
+    echo ${distri_name}
+}
+
+# Get distribution and bit
+get_os_info() {
+   echo $(get_os_distribution)
+}
+
+ditro=`get_os_info`
+if [ $ditro = "ubuntu" ]; then
+  sudo apt install -y build-essential
+  sudo apt install -y curl
+  sudo apt install -y vim
+  sudo apt install -y byobu
+else
+  echo
+fi
 
 curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
 sh ./installer.sh ~/.vim/dein
